6564b8c19793d1fe808f49c7de29143be301dab2
FrenzyR <rubenverrod@gmail.com>
2024-01-15 16:25:44 +0100

    Light test

--- Scripts/CharacterBody2D.cs
+++ Scripts/CharacterBody2D.cs
@@ -3,16 +3,24 @@ using System;
 
 public partial class CharacterBody2D : Godot.CharacterBody2D
 {
-	public const float Speed = 250.0f;
+	public const float Speed = 350.0f;
 	public const float JumpVelocity = -400.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	public Label thisLabel = GetNode("NormalLabel") as Label;
+
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
+
+		if (Input.GetActionStrength("Light") != 0)
+		{
+			this.thisLabel.setText("Light pressed");
+		}
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
